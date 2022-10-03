af569c215ae3ea24b8d438f4081131d055c7435e
Anurag Chitrakar <114130728+anuraggems07@users.noreply.github.com>
2022-10-04 01:34:42 +0530

    adding comments

--- script.sh
+++ script.sh
@@ -16,8 +16,10 @@
 # installing the required dependencies and files
 apt-get -y update
 apt-get -y install git
-echo "============git installed================="
+echo "==git installed=="
 git --version
+
+# To install Python from base
 apt update
 apt -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
 cd /tmp
@@ -28,43 +30,53 @@ cd Python-3.7.5
 make altinstall
 make install
 python3 --version
-echo "============Python installed================="
+echo "==Python installed=="
 cd ..
 cd ..
 apt-get -y install pip
-echo "====================== pip installed================="
+echo "== pip installed=="
 pip install --user annoy
+
+# numpy installation
 pip install numpy
 pip3 install --upgrade cython
-echo "====================== numpy installed================="
+echo "== numpy installed=="
+
+# installation of h5py module
 apt-get install -y pkg-config
 apt-get -y install libhdf5-dev
 pip install h5py
-pip install hererocks
-echo "====================== hererocks installed================="
 
-#apt-get -y install libreadline-dev
+pip install hererocks
+echo "== hererocks installed=="
+
 apt-get -y install curl
 hererocks here --lua 5.1 --luarocks 2.2
+
 export PATH="$(pwd)/here/bin/:$PATH"
+
 apt-get install zip unzip
 apt-get -y install luarocks
 luarocks install busted
 luarocks build mpack
 
 pip install swig
-echo "======================swig installed================="
+echo "==swig installed=="
 pip install tox
-echo "======================tox installed================="
+echo "===tox installed==="
 cd tmp/
-# Cloning Repo
+
+# Cloning the Repository
 git clone -q https://github.com/spotify/annoy.git C:\projects\annoy
 cd C:projectsannoy/
 
 # Build and test package
-echo "============build started================="
+echo "==build started=="
+
+# Build and install annoy
 luarocks make
-echo "============build setup.py started================="
+
+# To run the tests 
 python3 setup.py build
 python3 setup.py nosetests
 busted test/annoy_test.lua
